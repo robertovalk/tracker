9f71ca8001b5c6e03e440439ddf177301074b220
Roberto Falcão <rfalcaao@gmail.com>
2024-04-23 00:44:24 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <barraLateral />
+
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario_Principal />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import barraLateral from './components/barraLateral.vue';
+import Formulario_Principal from './components/Formulario_Principal.vue';
+
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    barraLateral,
+    Formulario_Principal
   }
+
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
